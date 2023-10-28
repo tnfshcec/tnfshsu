96c4035f5494f156e5b8779907f69f3eb270f6f9
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2023-10-28 12:42:41 +0800

    astro: v3 migration

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 
@@ -9,17 +8,11 @@ export default defineConfig({
   build: {
     inlineStylesheets: "auto",
   },
-  experimental: {
-    viewTransitions: true,
-  },
   integrations: [
     tailwind({
       config: {
         applyBaseStyles: false,
       },
-    }),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
     }),
     prefetch({
       selector: "a",
--- astro/src/utils/helpers.ts
+++ astro/src/utils/helpers.ts
@@ -1,25 +1,35 @@
-import { payloadSlateToDomConfig, slateToHtml } from "slate-serializers";
+import { slateToHtml } from "@slate-serializers/html";
+import {
+  payloadSlateToDomConfig,
+  type SlateToDomConfig,
+} from "@slate-serializers/dom";
 import { Element } from "domhandler";
 
+const payloadConfig: SlateToDomConfig = {
+  ...payloadSlateToDomConfig,
+  elementTransforms: {
+    ...payloadSlateToDomConfig.elementTransforms,
+    upload: ({ node }) =>
+      new Element("img", {
+        src: node.value.filename,
+        width: `${node.value.width}`,
+        height: `${node.value.height}`,
+      }),
+  },
+};
+
 export const getContentArray = (content: any) => {
-  const html = slateToHtml(content, {
-    ...payloadSlateToDomConfig,
-    elementTransforms: {
-      ...payloadSlateToDomConfig.elementTransforms,
-      upload: ({ node }) =>
-        // @ts-ignore
-        new Element("img", {
-          src: node.value.filename,
-          width: `${node.value.width}`,
-          height: `${node.value.height}`,
-        }),
-    },
-  }).replaceAll("<p></p>", "<p>&nbsp;</p>");
+  const html = slateToHtml(content, payloadConfig).replaceAll(
+    "<p></p>",
+    "<p>&nbsp;</p>",
+  );
+
   const htmlImageArray: (
     | string
     | { src: string; width: number; height: number }
   )[] = [];
   let lastIndex = 0;
+
   while (true) {
     const imgStartIndex = html.indexOf("<img", lastIndex);
     if (imgStartIndex === -1) {
