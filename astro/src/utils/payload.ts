1a22f321c1e262e7da5c8c5a2c4320350a326265
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2023-10-28 14:20:51 +0800

    remove usage of <Image /> component
    removed Astro <Image /> and replaced with HTML <img>
    as (1) remote images wouldn't be optimized
    and (2) the element extraction from HTML into component makes things
    complicated.
    
    as a result, the <Content /> component is replaced
    with a single <article />, and the extraction logic
    is removed.
    
    but at some point the images should still be processed
    through an Astro integration or something else.

--- astro/src/utils/payload.ts
+++ astro/src/utils/payload.ts
@@ -1,8 +1,14 @@
 import type { Post } from "@/types";
+import { Element } from "domhandler";
+import { slateToHtml } from "@slate-serializers/html";
+import {
+  payloadSlateToDomConfig,
+  type SlateToDomConfig,
+} from "@slate-serializers/dom";
 
-const url = import.meta.env.DEV
-  ? "http://payload:3001"
-  : `${import.meta.env.PAYLOAD_URL}`;
+const externalUrl = import.meta.env.PAYLOAD_URL;
+
+const url = import.meta.env.DEV ? "http://payload:3001" : externalUrl;
 
 export const getPosts = async () =>
   (await (await fetch(`${url}/api/posts`)).json()).docs as Post[];
@@ -10,4 +16,24 @@ export const getPosts = async () =>
 export const getPost = async (id: string) =>
   (await (await fetch(`${url}/api/posts/${id}`)).json()) as Post;
 
-export const getImageSrc = (src: string) => `${url}/media/${src}`;
+export const getImageSrc = (src: string) => `${externalUrl}/media/${src}`;
+
+const payloadConfig: SlateToDomConfig = {
+  ...payloadSlateToDomConfig,
+  elementTransforms: {
+    ...payloadSlateToDomConfig.elementTransforms,
+    upload: ({ node }) =>
+      new Element("img", {
+        src: getImageSrc(node.value.filename),
+        width: `${node.value.width}`,
+        height: `${node.value.height}`,
+        alt: node.value.alt,
+        decoding: "async",
+        loading: "lazy",
+      }),
+  },
+};
+
+export const getContent = (content: any) => {
+  return slateToHtml(content, payloadConfig);
+};
