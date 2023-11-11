b6ea54ad94b7127a23e38b8e32912704086ea7fe
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2023-11-11 19:00:35 +0800

    remove tailwindcss & cleanup dependencies

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,22 +1,10 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import prefetch from "@astrojs/prefetch";
 
 export default defineConfig({
   compressHTML: true,
   build: {
     inlineStylesheets: "auto",
   },
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    prefetch({
-      selector: "a",
-    }),
-    sitemap(),
-  ],
+  integrations: [sitemap()],
 });
