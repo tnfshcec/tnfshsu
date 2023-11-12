c0d964354fc4ec6577a529d60917f921cc8644dc
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2023-11-12 09:41:18 +0800

    update the api functions

--- astro/src/utils/payload.ts
+++ astro/src/utils/payload.ts
@@ -3,7 +3,7 @@ import type { Department, Faq, Post } from "@/types";
 const url = import.meta.env.DEV ? "http://payload:3001" : import.meta.env.PAYLOAD_URL;
 
 async function request(path: string) {
-  return (await fetch(`${url}/api/${path}`)).json();
+  return fetch(`${url}/api/${path}`).then((r) => r.json());
 }
 
 export async function getPosts(): Promise<Post[]> {
@@ -15,7 +15,7 @@ export async function getPost(id: string): Promise<Post> {
 }
 
 export async function getDepartments(): Promise<Department[]> {
-  return await request("departments").then((r) => r.docs);
+  return request("departments").then((r) => r.docs);
 }
 
 export async function getFaq(): Promise<Faq> {
